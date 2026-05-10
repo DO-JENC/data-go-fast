921a6b4fa042facf1a4de8f105d7384b596f5fc4
Jessy Latmi <jessy.latmi@etu.umontpellier.fr>
2026-05-10 12:00:07 +0200

    feat: added AppState

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,15 @@ mod handlers;
 use std::env;
 
 use crate::api::router::router as app_router;
+use common::infra::database::config::create_pool_from_env;
+use common::infra::s3::config::{S3Instance, init_s3_instance};
+use sqlx::{Pool, Postgres};
+
+#[derive(Clone)]
+pub struct AppState {
+  pub pool: Pool<Postgres>,
+  pub s3_instance: S3Instance,
+}
 
 #[tokio::main]
 async fn main() {
@@ -10,10 +19,15 @@ async fn main() {
   let addr = format!("0.0.0.0:{server_port}");
   let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
 
-  let database_url =
-    env::var("DATABASE_URL").expect("DATABASE_URL environment variable is not set");
-  let pool = sqlx::PgPool::connect(&database_url).await.unwrap();
+  let pool: Pool<Postgres> = create_pool_from_env().await.unwrap();
+  let s3_instance = init_s3_instance();
 
-  let router = app_router(pool);
+  let state: AppState = AppState {
+    pool: pool,
+    s3_instance: s3_instance,
+  };
+
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  let router = app_router(state);
   axum::serve(listener, router).await.unwrap();
 }
