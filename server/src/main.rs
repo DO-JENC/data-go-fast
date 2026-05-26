5c2668313f011a9f4f72aab3f3d3be443f928543
nadia.lahya <lalanana775@gmail.com>
2026-05-26 10:46:48 +0200

    refactor(server) : hardcoded db url removed ti use env file

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,18 @@
 mod api;
 mod handlers;
+use std::env;
 
 use crate::api::router::router as app_router;
 
 #[tokio::main]
 async fn main() {
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-  let pool = sqlx::PgPool::connect("postgresql://user:password@db/user")
+
+  if env::var("DATABASE_URL").is_err() {
+    panic!("DATABASE_URL environment variable is not set");
+  }
+
+  let pool = sqlx::PgPool::connect(&env::var("DATABASE_URL").unwrap())
     .await
     .unwrap();
   let router = app_router(pool);
