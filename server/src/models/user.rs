e95a4450ffce33a059e5a3253af7f37c797f935e
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-05-28 09:36:11 +0200

    feat(server/models/user): create user DTO

---
+++ server/src/models/user.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
+pub struct User {
+    pub id: Uuid,
+    pub email: String,
+    pub hash_password: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SignupRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct UserResponse {
+    pub id: Uuid,
+    pub email: String,
+}
+
+impl From<User> for UserResponse {
+    fn from(user: User) -> Self {
+        Self {
+            id: user.id,
+            email: user.email,
+        }
+    }
+}
