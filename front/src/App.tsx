8ffbc6061c57750214c7dd7ac69deb759fb83315
nadia.lahya <lalanana775@gmail.com>
2026-05-29 08:24:37 +0200

    feat(front): App layout, sticky sidebar, and global styles
    Co-authored-by: Eliott Bassier <eliottbassier14@gmail.com>

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,22 @@
-import Home from "@/pages/Home.tsx"
+import { Header } from "@/components/layout/header"
+import { Footer } from "@/components/layout/footer"
 import Signup from "@/pages/Signup.tsx"
+import Datasources from "@/pages/Datasources"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./App.css"
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <Header />
+      <main className="flex min-h-0 flex-1 flex-col bg-[var(--bg)]">
+        <Routes>
+          <Route path="/" element={<Datasources />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/datasources" element={<Datasources />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   )
 }
