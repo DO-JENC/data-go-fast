9d4e95a8075653d912b79001da523a4075835847
Jessy Latmi <jessy.latmi@etu.umontpellier.fr>
2026-06-02 21:44:28 +0200

    feat: init worker

--- server/src/handlers/jobs.rs
+++ server/src/handlers/jobs.rs
@@ -1,34 +1,39 @@
+use crate::AppState;
+use apalis::prelude::*;
+use apalis_core::request::Parts;
+use apalis_redis::{RedisContext, RedisStorage};
+use common::queue::models::{Job, Pipeline};
 use redis::{Client, JsonAsyncCommands, RedisResult, aio::MultiplexedConnection};
+use serde::{Deserialize, Serialize};
 use serde_json::{Value, json};
-use sqlx::{Error, Pool, Postgres, postgres::PgRow, query};
+use sqlx::{Error, Pool, Postgres, postgres::PgRow, query, types::Json};
 use uuid::Uuid;
 
 pub async fn add_job_to_redis(
-  pipeline: &Value,
+  redis_conn: &RedisStorage<Job>,
+  pipeline: &Pipeline,
   job_uuid: &Uuid,
   job_name: &str,
   datasource_s3_id: &str,
-) -> RedisResult<()> {
-  let redis_connection_string: String = std::env::var("REDIS_CONNECTION_STRING")
-    .expect("REDIS_CONNECTION_STRING environment variable not found.");
+) -> Result<Parts<RedisContext>, Error> {
+  let response: Parts<RedisContext> = redis_conn
+    .clone()
+    .push(Job {
+      job_id: *job_uuid,
+      datasource_id: datasource_s3_id.into(),
+      name: job_name.into(),
+      pipeline: pipeline.clone(),
+      status: "pending".into(),
+    })
+    .await
+    .expect("Failed to push job");
 
-  let client: Client = redis::Client::open(redis_connection_string)?;
-  let mut con: MultiplexedConnection = client.get_multiplexed_async_connection().await?;
-
-  let redis_json: Value = json!({
-      "name": job_name,
-      "status": "pending",
-      "datasource_id": datasource_s3_id,
-      "pipeline": pipeline
-  });
-
-  let response: () = con.json_set(job_uuid.to_string(), "$", &redis_json).await?;
   Ok(response)
 }
 
 pub async fn add_job_to_postgres(
   pool: &Pool<Postgres>,
-  pipeline: &Value,
+  pipeline: &Pipeline,
   job_uuid: &Uuid,
   job_name: &str,
   file_uuid: &Uuid,
@@ -36,7 +41,7 @@ pub async fn add_job_to_postgres(
   let job_row = query("INSERT INTO jobs VALUES ($1, $2, $3, 'pending') RETURNING *;")
     .bind(job_uuid)
     .bind(job_name)
-    .bind(pipeline)
+    .bind(Json(pipeline))
     .fetch_one(pool)
     .await?;
 
--- server/src/main.rs
+++ server/src/main.rs
@@ -4,22 +4,31 @@ mod infra;
 mod models;
 
 use crate::api::router::router as app_router;
+use apalis_redis::RedisStorage;
 use common::infra::database::config::create_pool_from_env;
 use common::infra::s3::config::{S3Instance, init_s3_instance};
+use common::queue::models::Job;
+use common::queue::storage::get_queue_storage;
 use sqlx::{Pool, Postgres};
 
 #[derive(Clone)]
 pub struct AppState {
   pub pool: Pool<Postgres>,
   pub s3_instance: S3Instance,
+  pub storage: RedisStorage<Job>,
 }
 
 #[tokio::main]
 async fn main() {
   let pool: Pool<Postgres> = create_pool_from_env().await.unwrap();
-  let s3_instance = init_s3_instance();
+  let s3_instance: S3Instance = init_s3_instance();
+  let storage: RedisStorage<Job> = get_queue_storage().await;
 
-  let state: AppState = AppState { pool, s3_instance };
+  let state: AppState = AppState {
+    pool,
+    s3_instance,
+    storage,
+  };
 
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
   let router = app_router(state);
