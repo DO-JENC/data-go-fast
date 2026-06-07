ec4dd51e2f4e3d94b58ce6f2c9e89a4de4f8a87d
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-07 17:47:30 +0200

    fix(front/signup): setup sign up form to use all the space (flex-1) + reduce padding slightly

--- front/src/pages/Signup.tsx
+++ front/src/pages/Signup.tsx
@@ -2,7 +2,7 @@ import { SignupForm } from "@/components/signup-form"
 
 export default function Signup() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+    <div className="flex flex-1 flex-col items-center justify-center p-4">
       <SignupForm />
     </div>
   )
