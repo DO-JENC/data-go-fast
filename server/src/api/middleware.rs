7e1d35f24210788646bcd7497de1ad6e760c96ea
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-07 14:52:07 +0200

    feat(server/handler): implement login & signup logic, with jwt generation

--- server/src/api/middleware.rs
+++ server/src/api/middleware.rs
@@ -1,7 +1,7 @@
 use crate::models::auth::Claims;
 use axum::{
-  extract::{FromRequestParts, Request},
-  http::{StatusCode, header, request::Parts},
+  extract::Request,
+  http::{StatusCode, header},
   middleware::Next,
   response::Response,
 };
@@ -36,20 +36,4 @@ pub async fn auth_middleware(mut req: Request, next: Next) -> Result<Response, S
 
   req.extensions_mut().insert(claims);
   Ok(next.run(req).await)
-}
-
-// Extractor
-pub struct AuthenticatedUser(pub Claims);
-
-impl<S: Send + Sync> FromRequestParts<S> for AuthenticatedUser {
-  type Rejection = StatusCode;
-
-  async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-    parts
-      .extensions
-      .get::<Claims>()
-      .cloned()
-      .map(AuthenticatedUser)
-      .ok_or(StatusCode::UNAUTHORIZED)
-  }
 }
