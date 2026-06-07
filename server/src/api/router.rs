f3fa320ba7e51a64c8565cf894373f92a63be514
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-07 14:52:07 +0200

    feat(server/handler): implement login & signup logic, with jwt generation

--- server/src/api/router.rs
+++ server/src/api/router.rs
@@ -20,6 +20,11 @@ use axum::{
 use tower_http::cors::{Any, CorsLayer};
 
 const FILE_SIZE_LIMIT: usize = 1024 * 1024 * 1024; // 1GB
+fn auth_router() -> Router<AppState> {
+  Router::new()
+    .route("/signup", post(signup))
+    .route("/login", post(login))
+}
 
 fn auth_router() -> Router<AppState> {
   Router::new()
