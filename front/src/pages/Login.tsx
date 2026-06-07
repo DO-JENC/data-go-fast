cb1ec51d4ec510f349cb7d538a41130bf82fa602
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-07 16:09:45 +0200

    feat(front): add login form page

---
+++ front/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/login-form"
+
+export default function Login() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <LoginForm />
+    </div>
+  )
+}
