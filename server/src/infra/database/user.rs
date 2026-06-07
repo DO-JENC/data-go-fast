8f4de6ddc37dc9cafc39e3676a313c91418fee4b
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-07 18:23:38 +0200

    feat(server/auth): implement /users/me endpoint

--- server/src/infra/database/user.rs
+++ server/src/infra/database/user.rs
@@ -26,3 +26,12 @@ pub async fn find_user_by_email(pool: &PgPool, email: &str) -> Result<Option<Use
 
   Ok(user)
 }
+
+pub async fn find_user_by_id(pool: &PgPool, id: uuid::Uuid) -> Result<Option<User>, sqlx::Error> {
+  let user = sqlx::query_as::<_, User>("SELECT id, email, hash_password FROM users WHERE id = $1")
+    .bind(id)
+    .fetch_optional(pool)
+    .await?;
+
+  Ok(user)
+}
