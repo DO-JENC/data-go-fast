a0e764ce168fc692666ae5cf952167bcc8d19942
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-07 14:52:07 +0200

    feat(server/handler): implement login & signup logic, with jwt generation

--- server/src/api/router.rs
+++ server/src/api/router.rs
@@ -1,16 +1,27 @@
-use crate::AppState;
-use crate::handlers::datasources::*;
-use crate::handlers::groups::*;
-use crate::handlers::jobs::*;
-use crate::handlers::users::*;
-use axum::extract::DefaultBodyLimit;
+use crate::{
+  AppState,
+  api::middleware::auth_middleware,
+  handlers::{
+    auth::{login, signup},
+    datasources::*,
+    groups::*,
+    jobs::{create_job_handler, list_jobs_handler, get_job_by_id_handler}
+  },
+};
+
 use axum::{
   Router,
   http::StatusCode,
+  middleware,
   routing::{delete, get, post},
 };
 
 const FILE_SIZE_LIMIT: usize = 1024 * 1024 * 1024; // 1GB
+fn auth_router() -> Router<AppState> {
+  Router::new()
+    .route("/signup", post(signup))
+    .route("/login", post(login))
+}
 
 fn datasources_router() -> Router<AppState> {
   Router::new()
@@ -19,6 +30,7 @@ fn datasources_router() -> Router<AppState> {
     .route("/", post(csv_ingestion_handler))
     .route("/{id}", delete(delete_datasource_by_id))
     .layer(DefaultBodyLimit::max(FILE_SIZE_LIMIT))
+    .layer(middleware::from_fn(auth_middleware))
 }
 
 fn jobs_router() -> Router<AppState> {
@@ -26,13 +38,14 @@ fn jobs_router() -> Router<AppState> {
     .route("/", post(create_job_handler))
     .route("/", get(list_jobs_handler))
     .route("/{id}", get(get_job_by_id_handler))
+    .layer(middleware::from_fn(auth_middleware))
 }
 
 fn users_router() -> Router<AppState> {
   Router::new()
-    .route("/signup", post(signup))
     .route("/", get(|| async { StatusCode::NOT_IMPLEMENTED }))
     .route("/{id}", get(|| async { StatusCode::NOT_IMPLEMENTED }))
+    .layer(middleware::from_fn(auth_middleware))
 }
 
 fn groups_router() -> Router<AppState> {
@@ -40,10 +53,12 @@ fn groups_router() -> Router<AppState> {
     .route("/", post(create_group_handler))
     .route("/{id}/join", post(join_group_handler))
     .route("/{id}/members", get(list_members_handler))
+    .layer(middleware::from_fn(auth_middleware))
 }
 pub fn router(state: AppState) -> Router {
   Router::new()
     .route("/health", get(|| async { StatusCode::OK }))
+    .nest("/auth", auth_router())
     .nest("/datasources", datasources_router())
     .nest("/jobs", jobs_router())
     .nest("/users", users_router())
