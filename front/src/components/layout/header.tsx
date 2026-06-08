ea823e1fa0f581582dbc54f0f5f4d7134ea9ae21
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-08 22:55:51 +0200

    feat(front/components/layout/header): implement logout behaviour

--- front/src/components/layout/header.tsx
+++ front/src/components/layout/header.tsx
@@ -9,29 +9,43 @@ import {
   CNavLink,
 } from "@coreui/react"
 import { LogOut } from "lucide-react"
+import { Link, useNavigate } from "react-router-dom"
 
 export function Header() {
-  const { user } = useAuth()
+  const { user, logout } = useAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = (e: React.MouseEvent) => {
+    e.preventDefault()
+    logout()
+    navigate("/login")
+  }
 
   return (
     <CHeader className="py-1">
       <CContainer fluid>
-        <CHeaderBrand href="#" className="flex items-center gap-3">
-          <img
-            src="/public/logo.png"
-            alt="Data-go-fast logo"
-            className="max-h-10"
-          />
-          <span className="font-heading text-xl font-bold text-[#8828ad]">
-            data-go-fast
-          </span>
-        </CHeaderBrand>
+        <Link to="/" className="flex items-center gap-3 no-underline!">
+          <CHeaderBrand className="flex items-center gap-3 m-0">
+            <img
+              src="/public/logo.png"
+              alt="Data-go-fast logo"
+              className="max-h-10"
+            />
+            <span className="font-heading text-xl font-bold text-[#8828ad]">
+              data-go-fast
+            </span>
+          </CHeaderBrand>
+        </Link>
 
         <CHeaderNav>
           {user && (
             <CNavItem>
-              {/* TODO: Logooout behaviour */}
-              <CNavLink href="#">
+              <CNavLink
+                href="#"
+                onClick={handleLogout}
+                title="Logout"
+                className="cursor-pointer"
+              >
                 <LogOut className="h-5 w-5 text-[#8828ad] transition-opacity hover:opacity-80" />
               </CNavLink>
             </CNavItem>
