055da7a3ba67e393e20522289c3fe8c4bf009d59
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-08 11:49:09 +0200

    feat(server/infra): add basic connection (non worker related) to redis for storing refresh token

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,11 +5,13 @@ mod infra;
 mod models;
 
 use crate::api::router::router as app_router;
+use crate::infra::redis::config::init_redis_connection;
 use apalis_redis::RedisStorage;
 use common::infra::database::config::create_pool_from_env;
 use common::infra::s3::config::{S3Instance, init_s3_instance};
 use common::queue::models::Job;
 use common::queue::storage::get_queue_storage;
+use redis::aio::ConnectionManager;
 use sqlx::{Pool, Postgres};
 
 #[derive(Clone)]
@@ -17,6 +19,7 @@ pub struct AppState {
   pub pool: Pool<Postgres>,
   pub s3_instance: S3Instance,
   pub storage: RedisStorage<Job>,
+  pub redis_connection: ConnectionManager,
 }
 
 #[tokio::main]
@@ -24,11 +27,15 @@ async fn main() {
   let pool: Pool<Postgres> = create_pool_from_env().await.unwrap();
   let s3_instance: S3Instance = init_s3_instance();
   let storage: RedisStorage<Job> = get_queue_storage().await;
+  let redis_connection = init_redis_connection()
+    .await
+    .expect("Failed to connect to redis.");
 
   let state: AppState = AppState {
     pool,
     s3_instance,
     storage,
+    redis_connection,
   };
 
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
