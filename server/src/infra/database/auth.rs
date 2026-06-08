f28f23299a33bd5b206d38bc654a57c1daeb0bb4
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-08 20:05:24 +0200

    feat(server): add refresh token route

--- server/src/infra/database/auth.rs
+++ server/src/infra/database/auth.rs
@@ -1,3 +1,4 @@
+use crate::models::auth::RefreshTokenRow;
 use chrono::{Duration, Utc};
 use sha2::{Digest, Sha256};
 use sqlx::PgPool;
@@ -11,13 +12,7 @@ pub async fn save_refresh_token(
   raw_token: &str,
   ttl_days: i64,
 ) -> Result<chrono::DateTime<Utc>, sqlx::Error> {
-  let mut hasher = Sha256::new();
-  hasher.update(raw_token.as_bytes());
-  let token_hash: String = hasher
-    .finalize()
-    .iter()
-    .map(|b| format!("{:02x}", b))
-    .collect();
+  let token_hash = get_hashed_token(raw_token);
 
   let expires_at = Utc::now() + Duration::days(ttl_days);
 
@@ -31,18 +26,37 @@ pub async fn save_refresh_token(
   Ok(expires_at)
 }
 
+pub async fn get_refresh_token(
+  pool: &PgPool,
+  raw_token: &str,
+) -> Result<Option<RefreshTokenRow>, sqlx::Error> {
+  let token_hash = get_hashed_token(raw_token);
+  let token_row = sqlx::query_as::<_, RefreshTokenRow>(
+    "SELECT id, user_id, expires_at FROM refresh_tokens WHERE token_hash=$1",
+  )
+  .bind(token_hash)
+  .fetch_optional(pool)
+  .await?;
+
+  Ok(token_row)
+}
+
 pub async fn revoke_refresh_token(pool: &PgPool, raw_token: &str) -> Result<(), sqlx::Error> {
-  let mut hasher = Sha256::new();
-  hasher.update(raw_token.as_bytes());
-  let token_hash: String = hasher
-    .finalize()
-    .iter()
-    .map(|b| format!("{:02x}", b))
-    .collect();
+  let token_hash: String = get_hashed_token(raw_token);
   sqlx::query("DELETE FROM refresh_tokens WHERE token_hash = $1")
     .bind(token_hash)
     .execute(pool)
     .await?;
 
   Ok(())
+}
+
+fn get_hashed_token(raw_token: &str) -> String {
+  let mut hasher = Sha256::new();
+  hasher.update(raw_token.as_bytes());
+  hasher
+    .finalize()
+    .iter()
+    .map(|b| format!("{:02x}", b))
+    .collect()
 }
