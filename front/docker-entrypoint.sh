6a4e6f307f2d9518a88bcfcaf3a051c773fe0a24
charlotte.lewis <charlotte.lewis@etu.umontpellier.fr>
2026-06-10 18:41:13 +0200

    fix(front): update VITE_API_URL to BACKEND_URL, fix nginx template copying

--- front/docker-entrypoint.sh
+++ front/docker-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-envsubst '${VITE_API_URL}' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/app.conf
+echo "BACKEND_URL is: $BACKEND_URL"
+envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/app.conf.template > /tmp/app.conf
+echo "Resolved config:"
 exec nginx -g "daemon off;"
